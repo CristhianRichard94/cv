7d045a9108b953006fd99fb60b10d90839394ab5
CristhianRichard94 <a1137854629>
2019-08-12 17:47:21 -0300

    add angular project

---
+++ src/app/shared/custom-material.module.ts
@@ -0,0 +1,34 @@
+import {NgModule} from '@angular/core';
+import {
+  MatButtonModule, MatCheckboxModule, MatPaginatorModule, MatSortModule,
+  MatTableModule, MatInputModule, MatGridListModule, MatDialogModule, MatTabsModule, MatSelectModule, MatAutocompleteModule,
+  MatSidenavModule, MatToolbarModule, MatIconModule, MatBottomSheetModule, MatBottomSheetRef
+} from '@angular/material';
+import {OverlayModule} from '@angular/cdk/overlay';
+import {CdkTableModule} from '@angular/cdk/table';
+
+@NgModule({
+  exports: [
+    MatTableModule,
+    CdkTableModule,
+    MatButtonModule,
+    OverlayModule,
+    MatPaginatorModule,
+    MatSortModule,
+    MatCheckboxModule,
+    MatInputModule,
+    MatGridListModule,
+    MatDialogModule,
+    MatTabsModule,
+    MatSelectModule,
+    MatAutocompleteModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatBottomSheetModule,
+  ]
+})
+export class CustomMaterialModule {
+}
